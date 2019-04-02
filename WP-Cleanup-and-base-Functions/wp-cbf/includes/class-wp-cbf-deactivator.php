16b385d1e9a703f11bab7f30c7fff384f2ec2a40
James Kenney <james@jjedmond.com>
2019-04-02 17:44:39 -0500

    Add files via upload

---
+++ WP-Cleanup-and-base-Functions/wp-cbf/includes/class-wp-cbf-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       plaza3design.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ * @author     James Kenney <james@jjedmond.com>
+ */
+class Wp_Cbf_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
